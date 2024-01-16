32ba5781660e22b752fd8b6e65367317b2e1b11e
androidsdreamofelectricsheep <n01077202@humbermail.ca>
2024-01-16 17:18:09 +0900

    도메인 레포, 유즈케이스

---
+++ lib/features/guide_yongsan/domain/repositories/guide_yongsan_repository.dart
@@ -0,0 +1,22 @@
+import 'package:dartz/dartz.dart';
+import 'package:guide_yongsan/core/errors/failure.dart';
+import 'package:guide_yongsan/core/params/params.dart';
+import 'package:guide_yongsan/features/guide_yongsan/domain/entities/company_detail_info_entity.dart';
+import 'package:guide_yongsan/features/guide_yongsan/domain/entities/main_info_entity.dart';
+import 'package:guide_yongsan/features/guide_yongsan/domain/entities/marjor_category_entity.dart';
+import 'package:guide_yongsan/features/guide_yongsan/domain/entities/medium_category_entity.dart';
+
+abstract class GuideYongsanRepository {
+  // 대분류 카테고리
+  Future<Either<Failure, MarjorCategoryEntity>> getMajorCategory(
+      {required BaseParams params});
+  // 중분류 카테고리
+  Future<Either<Failure, MediumCategoryEntity>> getMediumCategory(
+      {required MediumCategoryParams params});
+  // 편의시설/업체 정보
+  Future<Either<Failure, MainInfoEntity>> getMainInfo(
+      {required MainInfoParams params});
+  // 시설/업체 세부정보
+  Future<Either<Failure, CompanyDetailInfoEntity>> getCompanyDetailInfo(
+      {required CompanyDetailInfoParams params});
+}
