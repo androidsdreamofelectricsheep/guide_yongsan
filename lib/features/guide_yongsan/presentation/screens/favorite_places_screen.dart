1fb20789696a6992e2a54e473a954dcba6d19126
androidsdreamofelectricsheep <n01077202@humbermail.ca>
2024-05-09 15:30:11 +0900

    좋아요한 장소 없을 시 버튼 노출 및 문구 표기(구글 플레이 QA)

--- lib/features/guide_yongsan/presentation/screens/favorite_places_screen.dart
+++ lib/features/guide_yongsan/presentation/screens/favorite_places_screen.dart
@@ -87,81 +87,90 @@ class _FavoritePlacesScreenState extends State<FavoritePlacesScreen> {
     return BaseLayout(
       appBarTitle: 'Favorite places',
       child: Column(children: [
-        Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            Transform.translate(
-              offset: const Offset(4, 0),
-              child: TextButton.icon(
-                  onPressed: onSelectAllPlaces,
-                  icon: Icon(isSelectedAll
-                      ? Icons.check_circle_rounded
-                      : Icons.check_circle_outline_rounded),
-                  label: const Text('Select all')),
-            ),
-            // 개별 장소 체크 버튼은 아이콘 버튼으로 됨, 텍스트 버튼보다 더 큰 자체 패딩을 가지고 있어서 수직 정렬이 맞지 않음
-            // 이를 맞추기 위함
-            Transform.translate(
-              offset: const Offset(-4, 0),
-              child: TextButton.icon(
-                  onPressed: onDeleteSelectedPlaces,
-                  icon: Icon(removePlaceList.isEmpty
-                      ? Icons.delete_outline_rounded
-                      : Icons.delete_forever_rounded),
-                  label: const Text('Delete')),
-            )
-          ],
-        ),
-        Expanded(
-          child: ListView.builder(
-              scrollDirection: Axis.vertical,
-              shrinkWrap: true,
-              itemCount: favoritePlaces.length,
-              itemBuilder: (context, index) => FavoritePlaceListItemWidget(
-                    companyId: favoritePlaces[index]['companyId'],
-                    companyName: favoritePlaces[index]['companyName'],
-                    keyWord: favoritePlaces[index]['keyWord'] ?? '',
-                    addr: favoritePlaces[index]['addr'] ?? '',
-                    pointLat: favoritePlaces[index]['pointLat'],
-                    pointLng: favoritePlaces[index]['pointLng'],
-                    value: forAll
-                        ? favoritePlaces[index]['value'] =
-                            isSelectedAll // 전체 선택 플래그 체크시 전체 선택 값, 각각의 모든 장소 체크 박스에 할당(모두 체크, 모두 해제)
-                        : favoritePlaces[index]['value'],
-                    onChange: () {
-                      setState(() {
-                        forAll = false; // 각각의 좋아요한 장소 체크박스 체크시 전체 선택 플래그 해제
-                      });
+        favoritePlaces.isNotEmpty
+            ? Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Transform.translate(
+                    offset: const Offset(4, 0),
+                    child: TextButton.icon(
+                        onPressed: onSelectAllPlaces,
+                        icon: Icon(isSelectedAll
+                            ? Icons.check_circle_rounded
+                            : Icons.check_circle_outline_rounded),
+                        label: const Text('Select all')),
+                  ),
+                  // 개별 장소 체크 버튼은 아이콘 버튼으로 됨, 텍스트 버튼보다 더 큰 자체 패딩을 가지고 있어서 수직 정렬이 맞지 않음
+                  // 이를 맞추기 위함
+                  Transform.translate(
+                    offset: const Offset(-4, 0),
+                    child: TextButton.icon(
+                        onPressed: onDeleteSelectedPlaces,
+                        icon: Icon(removePlaceList.isEmpty
+                            ? Icons.delete_outline_rounded
+                            : Icons.delete_forever_rounded),
+                        label: const Text('Delete')),
+                  )
+                ],
+              )
+            : const SizedBox.shrink(),
+        favoritePlaces.isNotEmpty
+            ? Expanded(
+                child: ListView.builder(
+                    scrollDirection: Axis.vertical,
+                    shrinkWrap: true,
+                    itemCount: favoritePlaces.length,
+                    itemBuilder: (context, index) =>
+                        FavoritePlaceListItemWidget(
+                          companyId: favoritePlaces[index]['companyId'],
+                          companyName: favoritePlaces[index]['companyName'],
+                          keyWord: favoritePlaces[index]['keyWord'] ?? '',
+                          addr: favoritePlaces[index]['addr'] ?? '',
+                          pointLat: favoritePlaces[index]['pointLat'],
+                          pointLng: favoritePlaces[index]['pointLng'],
+                          value: forAll
+                              ? favoritePlaces[index]['value'] =
+                                  isSelectedAll // 전체 선택 플래그 체크시 전체 선택 값, 각각의 모든 장소 체크 박스에 할당(모두 체크, 모두 해제)
+                              : favoritePlaces[index]['value'],
+                          onChange: () {
+                            setState(() {
+                              forAll =
+                                  false; // 각각의 좋아요한 장소 체크박스 체크시 전체 선택 플래그 해제
+                            });
 
-                      final newValue = !favoritePlaces[index]['value'];
-                      favoritePlaces[index]['value'] = newValue;
+                            final newValue = !favoritePlaces[index]['value'];
+                            favoritePlaces[index]['value'] = newValue;
 
-                      if (!newValue) {
-                        setState(() {
-                          isSelectedAll = false;
-                        });
-                      } else {
-                        final allSelected =
-                            favoritePlaces.every((place) => place['value']);
+                            if (!newValue) {
+                              setState(() {
+                                isSelectedAll = false;
+                              });
+                            } else {
+                              final allSelected = favoritePlaces
+                                  .every((place) => place['value']);
 
-                        setState(() {
-                          isSelectedAll = allSelected;
-                        });
-                      }
+                              setState(() {
+                                isSelectedAll = allSelected;
+                              });
+                            }
 
-                      if (favoritePlaces[index]['value'] &&
-                          !removePlaceList
-                              .contains(favoritePlaces[index]['companyId'])) {
-                        // 체크되었을 때 삭제 리스트에 추가
-                        removePlaceList.add(favoritePlaces[index]['companyId']);
-                      } else {
-                        // 체크 해제 시 삭제 리스트에서 제거
-                        removePlaceList
-                            .remove(favoritePlaces[index]['companyId']);
-                      }
-                    },
-                  )),
-        )
+                            if (favoritePlaces[index]['value'] &&
+                                !removePlaceList.contains(
+                                    favoritePlaces[index]['companyId'])) {
+                              // 체크되었을 때 삭제 리스트에 추가
+                              removePlaceList
+                                  .add(favoritePlaces[index]['companyId']);
+                            } else {
+                              // 체크 해제 시 삭제 리스트에서 제거
+                              removePlaceList
+                                  .remove(favoritePlaces[index]['companyId']);
+                            }
+                          },
+                        )),
+              )
+            : const Center(
+                child: Text('There is no favorite places stored'),
+              )
       ]),
     );
   }
